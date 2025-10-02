25dc7af98b11849209bce5e83de1585b50b7a5b5
Ali <hisseiny@outlook.fr>
2025-10-02 13:10:21 +0000

    module1 python

---
+++ main.py
@@ -0,0 +1,88 @@
+# # TODO: Create an integer variable named 'age' with your age
+# age = 24
+
+# # TODO: Create a float variable named 'height' with your height in meters
+# height = 1.83
+
+# # TODO: Create a string variable named 'name' with your name
+# name = "Hissein"
+# # TODO: Create a boolean variable named 'is_student' and set it to True
+# is_student = True
+
+# # Print all variables and their types
+# print(f"Age:{age}, Type{type(age)} Height:{height}, Type:{type(height)},Name:{name}, Type{type(name)}, Student:{is_student}, Type{type(is_student)}")
+# # Example: print(f"Age: {age}, Type: {type(age)}")
+
+# TODO: Create two variables 'a' and 'b' with numeric values
+# a = 2
+# b = 3
+
+
+# # TODO: Perform addition, subtraction, multiplication, and division of a and b
+# a + b
+# a - b
+# a * b
+# a / b
+# # Print the results of each operation
+# print("Addition", a+b)
+# print("Sub", a-b)
+# print("Mul", a*b)
+# print("Devise", a/b)
+# print("Modulo", a % b)
+# print("power", a**b)
+
+# TODO: Calculate the remainder of a divided by b using the modulo operator
+
+# TODO: Calculate a to the power of b using the exponentiation operator
+
+# TODO: Create two string variables 'first_name' and 'last_name'
+
+# first_name = "Ali"
+# last_name = "Hissein"
+
+# # TODO: Concatenate the two names to create a 'full_name' variable
+# full_name = first_name + last_name
+# # TODO: Print the full name in uppercase
+# print(full_name.upper())
+# # TODO: Print the length of the full name
+# print(len(full_name))
+# # TODO: Create a string with multiple words and split it into a list
+# phrase = "welcome to Eugenia"
+# print(phrase.split())
+
+# TODO: Convert a string containing a number to an integer
+# a = int("123")
+# # TODO: Convert a float to an integer
+# b = int(1.25)
+
+# # TODO: Convert an integer to a float
+
+# c = float(24)
+
+# # TODO: Convert a number (integer or float) to a string
+# d = str(999)
+
+# # Print all converted values and their new types
+# print(f"a :{a}, Type {type(a)}")
+# print(f"b :{b}, Type {type(b)}")
+# print(f"a :{c}, Type {type(c)}")
+# print(f"a :{d}, Type {type(d)}")
+
+# TODO: Create two boolean variables
+right = True
+wrong = False
+
+# TODO: Perform AND, OR, and NOT operations on these variables
+a = right and wrong
+b = right or wrong
+c = not right
+d = not wrong
+# Print the results
+print(a, b, c, d)
+
+# TODO: Create two numeric variables and use comparison operators
+# (>, <, >=, <=, ==, !=) to compare them
+# Print the results of these comparisons
+a = 10
+b = 9
+print(a > b, a < b, a >= b, a <= b, a == b, a != b)
